47dc423a0ab8125c8aa0b6ee424f0f03d00f43a2
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2024-01-12 20:00:03 +0200

    get active cards

--- main.py
+++ main.py
@@ -9,5 +9,22 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
     cards = Passcard.objects.all()
-    print(cards)
+    some_name = cards[0]
+    owner_name = some_name.owner_name
+    passcode = some_name.passcode
+    created_at = some_name.created_at
+    is_active = some_name.is_active
+    # print("owner_name:", owner_name)
+    # print("passcode:", passcode)
+    # print("created_at:", created_at)
+    # print("is_active:", is_active)
+    # active_passcards = []
+    # for card in cards:
+    #     if card.is_active:
+    #         active_passcards.append(card)
+    active_passcards = Passcard.objects.filter(is_active=True)
+    print("Всего пропусков", len(cards))
+    print("Активных пропусков", len(active_passcards))
+
+
     # print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
