260d87ee29019dcbe98fa3ecafc50a6862a5bf69
NovemberJuliett <137638315+NovemberJuliett@users.noreply.github.com>
2024-01-14 21:05:22 +0200

    start working with datetime

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import os
-
 import django
+import datetime
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard
+from datacenter.models import Visit  # noqa: E402
+from django.utils.timezone import localtime
+
 
 if __name__ == '__main__':
     cards = Passcard.objects.all()
@@ -14,17 +18,20 @@ if __name__ == '__main__':
     passcode = some_name.passcode
     created_at = some_name.created_at
     is_active = some_name.is_active
-    # print("owner_name:", owner_name)
-    # print("passcode:", passcode)
-    # print("created_at:", created_at)
-    # print("is_active:", is_active)
-    # active_passcards = []
-    # for card in cards:
-    #     if card.is_active:
-    #         active_passcards.append(card)
+
     active_passcards = Passcard.objects.filter(is_active=True)
-    print("Всего пропусков", len(cards))
-    print("Активных пропусков", len(active_passcards))
+    # print("Всего пропусков", len(cards))
+    # print("Активных пропусков", len(active_passcards))
+
+    not_closed_visits = Visit.objects.filter(leaved_at__isnull=True)
+    # print(not_closed_visits)
+    local_time = localtime()
+    entered_at = Visit.objects.filter(entered_at__isnull=False)
+    print(entered_at)
+    for person in entered_at:
+        print("Зашёл в хранилище, время по Москве:", local_time)
+        delta = local_time - entered_at
+        print("Находится в хранилище:", delta.hour)
 
 
     # print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
